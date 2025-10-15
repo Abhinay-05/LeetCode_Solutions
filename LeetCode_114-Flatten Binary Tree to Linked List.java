f152e78bb2b67f0d48c9ae75189d8055c117060f
Abhinay-05 <abhinaynnd05@gmail.com>
2025-10-16 02:18:33 +0530

    Create LeetCode_114-Flatten Binary Tree to Linked List.java
    114. Flatten Binary Tree to Linked List
    
    Given the root of a binary tree, flatten the tree into a "linked list":
    
    The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
    The "linked list" should be in the same order as a pre-order traversal of the binary tree.

---
+++ LeetCode_114-Flatten Binary Tree to Linked List.java
@@ -0,0 +1,82 @@
+/*
+114. Flatten Binary Tree to Linked List
+
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+ 
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // solving pre-order using post-order in backtracking
+    public void flatten(TreeNode node) {
+        if(node == null){
+            return ;
+        }
+
+        // go to the right end first
+        flatten(node.right);
+        // go to the left end afterwards
+        flatten(node.left);
+
+        // if the left side has more children
+        // so get to the last (right)node 
+        // set that last node's right as the node.right
+        TreeNode prev = node.left;
+        if(prev != null){
+            while(prev.right != null){
+                prev = prev.right;
+            }
+            prev.right = node.right;
+        }
+
+        // if only right exists
+        // so that it does not skips the right
+        if(node.left != null){
+            node.right = node.left;
+        }
+        // left should be set to NULL for every node
+        node.left = null;
+    }
+}

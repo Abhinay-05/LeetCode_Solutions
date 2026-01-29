3e22a63cc66b7f3b43a438d78941c9c75c5cec4c
Abhinay-05 <abhinaynnd05@gmail.com>
2026-01-29 14:22:13 +0530

    Add solution for Kth Missing Positive Number
    Implement a solution to find the kth missing positive integer from a sorted array.

---
+++ LeetCode_1539-KthMissingPositiveNumber.java
@@ -0,0 +1,46 @@
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+
+Example 1:
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+Example 2:
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6. 
+
+Constraints:
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+ 
+Follow up:
+Could you solve this problem in less than O(n) complexity?
+*/
+
+class Solution {
+    //Binary Search
+    // TC: O(logN)
+    // SC: O(1)
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int missingNums = arr[mid] - (mid+1);
+
+            if(missingNums < k){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+
+        return (high+1)+k;//(high+1) is the number that should've been there
+    }
+}
